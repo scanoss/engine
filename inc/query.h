6cbb00baa86e2eaea2acd2212ac03db26c2bacc9
scanoss-cs <info@scanoss.com>
2021-08-30 15:44:30 +0200

    adds purl@version dependency check

--- inc/query.h
+++ inc/query.h
@@ -6,5 +6,6 @@
 /* Obtain the first available url record for the given MD5 hash */
 void get_url_record(uint8_t *md5, uint8_t *record);
 void extract_csv(char *out, char *in, int n, long limit);
+void purl_version_md5(uint8_t *out, char *purl, char *version);
 
 #endif
