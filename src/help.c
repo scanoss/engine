5eb3fae8e1807fedaf18fd9a01e44d205b444983
Core Software Developer <info@scanoss.com>
2022-12-29 13:30:48 +0100

    Correct typo in help, remove some unused declarations

--- src/help.c
+++ src/help.c
@@ -70,23 +70,23 @@ The scanning engine can be configured by passing configuration flags with the -F
 Alternatively, these value can be written in %s\n\
 +-------+-----------------------------------------------------+\n\
 | Flag  | Setting                                             |\n\
-+-------+-----------------------------------------------------+\n\
-|    1  | Disable snippet matching (default: enabled)         |\n\
-|    2  | Enable snippet_ids (default: disabled)              |\n\
-|    4  | Disable dependencies (default: enabled)             |\n\
-|    8  | Disable licenses (default: enabled)                 |\n\
-|   16  | Disable copyrights (default: enabled)               |\n\
-|   32  | Disable vulnerabilities (default: enabled)          |\n\
-|   64  | Disable quality (default: enabled)                  |\n\
-|  128  | Disable criptography (defalt: enabled)              |\n\
-|  256  | Disable best match only (default: enabled)          |\n\
-|  512  | Hide identified files (default: disabled)           |\n\
-| 1024  | Enable download_url (default: disabled)             |\n\
-| 2048  | Enable GitHub full path (default: disabled)         |\n\
-| 4096  | Disable extended server stats (default: enabled)    |\n\
-| 8192  | Disable helth layer (default: enabled)              |\n\
-| 16384 | Enable high accuray, slower scan (default: enabled) |\n\
-+-------+-----------------------------------------------------+\n\
++-------+-------------------------------------------------------+\n\
+|    1  | Disable snippet matching (default: enabled)           |\n\
+|    2  | Enable snippet_ids (default: disabled)                |\n\
+|    4  | Disable dependencies (default: enabled)               |\n\
+|    8  | Disable licenses (default: enabled)                   |\n\
+|   16  | Disable copyrights (default: enabled)                 |\n\
+|   32  | Disable vulnerabilities (default: enabled)            |\n\
+|   64  | Disable quality (default: enabled)                    |\n\
+|  128  | Disable criptography (defalt: enabled)                |\n\
+|  256  | Disable best match only (default: enabled)            |\n\
+|  512  | Hide identified files (default: disabled)             |\n\
+| 1024  | Enable download_url (default: disabled)               |\n\
+| 2048  | Enable GitHub full path (default: disabled)           |\n\
+| 4096  | Disable extended server stats (default: enabled)      |\n\
+| 8192  | Disable helth layer (default: enabled)                |\n\
+| 16384 | Enable high accuracy, slower scan (default: disabled) |\n\
++-------+-------------------------------------------------------+\n\
 Example: scanoss -F 12 DIRECTORY (scans DIRECTORY disabling license and dependency data)\n\
 \n\
 Copyright (C) 2018-2022 SCANOSS.COM\n", ENGINE_FLAGS_FILE);
--- src/limits.c
+++ src/limits.c
@@ -9,7 +9,6 @@
   * @see https://github.com/scanoss/engine/blob/master/src/limits.c
   */
 
-int scan_limit = 25; /** //Maximum matches to be processed */
 
 int consecutive_score = 4000;   /** Maximumm sUsed for snippet selection */
 
@@ -22,7 +21,5 @@ int consecutive_threshold = 50;
 int range_tolerance = 5;  /** A maximum number of non-matched lines tolerated inside a matching range */
 int min_match_lines = 10; /** Minimum number of lines matched for a match range to be acepted */
 int min_match_hits  = 5;  /** Minimum number of snippet ID hits to produce a snippet match*/
-
-const int rank_items = 1000; /** Number of items to evaluate in component and path rankings */
 
 const int max_vulnerabilities = 50; /** Show only the first N vulnerabilities */
